ea1b8706058d41035da93d15e82700887ebc3f00
rasya109 <satriavega2004@gmail.com>
2024-11-30 18:12:26 +0700

    feature(login): added login feature for user

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name' => $googleUser->getName(),
+                'google_id' => $googleUser->getId(),
+                'profile_picture' => $googleUser->getAvatar(),
+                'role_id' => 2, 
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
+    }
+}
+
