ca6acc38e92a2b370e0f05ac2b63fcc493198584
rasya109 <satriavega2004@gmail.com>
2024-12-05 11:52:09 +0700

    pull shandy

---
+++ app/Http/Controllers/AI/DestinationRecommendationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\AI;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Gemini\Laravel\Facades\Gemini;
+
+class DestinationRecommendationController extends Controller
+{
+    public function getDestinationRecommendation(Request $request)
+    {
+        //
+    }
+}
